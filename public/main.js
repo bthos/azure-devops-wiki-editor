583b2baa595fa5d3cc17c08f5f7c2d7df1bf5115
bthos <13679349+bthos@users.noreply.github.com>
2024-06-25 18:03:22 +0200

    Update public/main.js

--- public/main.js
+++ public/main.js
@@ -1,11 +1,9 @@
 var enableFilter = true;
 
-function whenElementAppear()
-{
-    if($(".we-ta-container textarea:visible").length>0)
-    {
+function whenElementAppear() {
+    if ($(".we-ta-container textarea:visible").length > 0) {
         var content = $(".we-ta-container textarea").val();
-        $(".wiki-editor .we-text-preview-container").css("display","none");
+        $(".wiki-editor .we-text-preview-container").css("display", "none");
         $(".wiki-editor").append("<div id='new-editor'></div>");
         var editor = new toastui.Editor({
             el: document.querySelector('#new-editor'),
@@ -15,19 +13,37 @@ function whenElementAppear()
             initialValue: content,
             events: {
                 change: contentChanged
+            },
+            hooks: {
+                addImageBlobHook: function (blob, callback) {
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        callback(e.target.result, blob.name);
+                    };
+                    reader.readAsDataURL(blob);
+                },
+                renderMarkdown: handleMentionsInTables
             }
         });
 
-        function contentChanged()
-        {
+        function contentChanged() {
             $(".we-ta-container textarea").val(editor.getMarkdown());
-            $(".we-ta-container textarea")[0].dispatchEvent(new Event('input', { bubbles: true}));
+            $(".we-ta-container textarea")[0].dispatchEvent(new Event('input', { bubbles: true }));
+        }
+
+        function handleMentionsInTables(markdown) {
+            // Regular expression to match @ mentions within tables
+            var mentionRegex = /(\|[^|]*?)@(\w+)([^|]*?\|)/g;
+            // Replace mentions with a span tag to avoid disrupting table formatting
+            return markdown.replace(mentionRegex, function (match, p1, p2, p3) {
+                return `${p1}<span class="mention">@${p2}</span>${p3}`;
+            });
         }
     }
 
     setTimeout(whenElementAppear, 500);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     whenElementAppear();
 });
