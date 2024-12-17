2372048a993fc5893079a66ae3730b046ed3a306
bthos <bthos@example.com>
2024-12-17 21:20:17 +0100

    chore: Remove unutilized code

--- public/main.js
+++ public/main.js
@@ -1,53 +1,36 @@
-var enableFilter = true;
+const enableFilter = true;
 
-function whenElementAppear()
-{
-    if($(".we-ta-container textarea:visible").length>0)
-    {
-        var content = $(".we-ta-container textarea").val();
-        $(".wiki-editor .we-text-preview-container").css("display","none");
+const initializeEditor = (content) => {
+    const editor = new toastui.Editor({
+        el: document.querySelector('#new-editor'),
+        height: 'auto',
+        initialEditType: 'wysiwyg',
+        previewStyle: 'vertical',
+        initialValue: content,
+        events: {
+            change: () => contentChanged(editor)
+        },
+        usageStatistics: false
+    });
+};
+
+const contentChanged = (editor) => {
+    const textarea = $(".we-ta-container textarea");
+    textarea.val(editor.getMarkdown());
+    textarea[0].dispatchEvent(new Event('input', { bubbles: true }));
+};
+
+const whenElementAppear = () => {
+    const textarea = $(".we-ta-container textarea:visible");
+    if (textarea.length > 0) {
+        const content = textarea.val();
+        $(".wiki-editor .we-text-preview-container").css("display", "none");
         $(".wiki-editor").append("<div id='new-editor'></div>");
-        var editor = new toastui.Editor({
-            el: document.querySelector('#new-editor'),
-            height: 'auto',
-            initialEditType: 'wysiwyg',
-            previewStyle: 'vertical',
-            initialValue: content,
-            events: {
-                change: contentChanged
-            },
-            customHTMLRenderer: {
-                emph: (node, context) => {
-                    if (node.literal === '[[_TOC_]]') {
-                        return entering ? '[[_TOC_]]' : '';
-                    }
-                    return context ? '**' : '**';
-                }
-            },
-            usageStatistics: false
-        });
-
-        function contentChanged()
-        {
-            $(".we-ta-container textarea").val(editor.getMarkdown());
-            $(".we-ta-container textarea")[0].dispatchEvent(new Event('input', { bubbles: true}));
-        }
-
-        // Add custom buttons to switch between edit types
-        var markdownButton = createApplyButton(i18n.get('Markdown'));
-        markdownButton.addEventListener('click', () => {
-                editor.changeMode('markdown');
-            });
-        var wysiwygButton = $('<button>').text('WYSIWYG').click(function() {
-                editor.changeMode('wysiwyg');
-            });
-        $('.toastui-editor-header').prepend(markdownButton, wysiwygButton);
+        initializeEditor(content);
     }
-
     setTimeout(whenElementAppear, 500);
-}
+};
 
-$(document).ready(function(){
+$(document).ready(() => {
     whenElementAppear();
 });
-
